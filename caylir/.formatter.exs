55a0c3be1825dccd4247d5726beee8dc801abd3d
Rob <rm.rf.etc@gmail.com>
2019-11-20 18:10:37 -0800

    Replace local copy of Caylir with github ref

--- caylir/.formatter.exs
+++
@@ -1,6 +0,0 @@
-[
-  inputs: [
-    "{lib,test}/**/*.{ex,exs}",
-    "{.formatter,mix}.exs"
-  ]
-]
--- caylir/test/caylir/graph/url_test.exs
+++
@@ -1,5 +0,0 @@
-defmodule Caylir.Graph.URLTest do
-  use ExUnit.Case, async: true
-
-  doctest Caylir.Graph.URL, import: true
-end
